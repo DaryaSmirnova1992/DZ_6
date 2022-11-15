98388f7b60bbdebc9ede680bef99dbaa91df97cb
Darya <darecc@yandex.ru>
2022-11-15 12:45:18 +0400

    add task_3

---
+++ task_3.py
@@ -0,0 +1,15 @@
+# Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+# txt = input("Введите текст через пробел:\n")
+# print(f"Исходный текст: {txt}")
+# find_txt = "абв"
+# lst = [i for i in txt.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
+
+# улучшение
+
+txt = input("Введите текст через пробел:\n")
+print(f"Исходный текст: {txt}")
+
+txt =' '.join(filter(lambda x: not'абв' in x, txt.lower().split()))
+print(txt)
