ea36aa07266c40174e6ed185b6bb96c95e0b83ed
Darya <darecc@yandex.ru>
2022-11-15 12:55:17 +0400

    add task_2

--- task_2.py
+++ task_2.py
@@ -1,13 +1,13 @@
 # Подсчитать сумму цифр в вещественном числе.
 
-number = float(input("Введите вещественное число: "))
+# number = float(input("Введите вещественное число: "))
 
-lst = list(str(number).split('.'))
-summ = 0
-for i in lst:
-    for j in i:
-        summ += int(j)
-print(f"Сумма цифр вещественного числа равна = {summ}")
+# lst = list(str(number).split('.'))
+# summ = 0
+# for i in lst:
+#     for j in i:
+#         summ += int(j)
+# print(f"Сумма цифр вещественного числа равна = {summ}")
 
 # улучшение
 
